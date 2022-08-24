af4b1b0ffd9d97b2d1cefefadc0f9e68796e8f02
Ali Ahmad @ Jetson <ali.ahmad@xavor.com>
2022-08-24 13:22:32 +0500

    Modifications for adding Movebase client inside the search person node. Currently not working on actual robot.

--- behavior_tree_leaves/example_nodes/cpp/demotree2/search_person.cpp
+++ behavior_tree_leaves/example_nodes/cpp/demotree2/search_person.cpp
@@ -26,7 +26,7 @@
 #include "geometry_msgs/Twist.h"
 #include<sstream>
 #include<vector>
-
+#include<rehab_person_loc/location_info.h>
 // using namespace std;
 
 enum Status {RUNNING, SUCCESS, FAILURE};
@@ -41,8 +41,8 @@ protected:
     // NodeHandle instance must be created before this line. Otherwise strange errors may occur.
     actionlib::SimpleActionServer<behavior_tree_core::BTAction> as_;
     std::string action_name_;
-    // MoveBaseClient ac;
-    // move_base_msgs::MoveBaseGoal move_base_goal;
+    MoveBaseClient ac;
+    move_base_msgs::MoveBaseGoal move_base_goal;
     // create messages that are used to published feedback/result
     behavior_tree_core::BTFeedback feedback_;
     behavior_tree_core::BTResult result_;
@@ -53,12 +53,14 @@ protected:
     std_msgs::String feedback_msg;
     bool publish_once = true;
     bool person_distance_threshold_high = false; // Initial false 
-
+    geometry_msgs::PointStamped person_loc_in_MAP;
+    bool person_inside_map = false;
 
  public:
 
-    explicit BTAction(std::string name) :
-    as_(nh_, name, boost::bind(&BTAction::execute_callback, this, _1), false),
+    //explicit BTAction(std::string name) : as_(nh_, name, boost::bind(&BTAction::execute_callback, this, _1), false),
+    //action_name_(name)
+    explicit BTAction(std::string name) : ac("move_base", true),  as_(nh_, name, boost::bind(&BTAction::execute_callback, this, _1), false),
     action_name_(name)
     {
         // start the action server (action in sense of Actionlib not BT action)
@@ -72,10 +74,15 @@ protected:
 
     ros::Subscriber point_sub = nh_.subscribe("/person_loc",1000, &BTAction::cameraCallBack,this);
     
+    ros::Subscriber person_point_sub = nh_.subscribe("/person_loc_estimated",1000, &BTAction::personinMAPCallBack,this);
+
+    ros::Subscriber loc_tag = nh_.subscribe("/location_tag",1000, &BTAction::locationTagMAPCallBack,this);
+
+
+
     ros::Publisher pub_vel = nh_.advertise<geometry_msgs::Twist>("/mobile_base_controller/cmd_vel", 1000); //For Actual Hardware
     //ros::Publisher pub_vel = nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 1000); //For simulation only
-    
-    
+
     ros::Publisher pub_feedback = nh_.advertise<std_msgs::String>("/feedback_to_tablet", 1000); //Publsiher for Feedback to Tablet
 
 
@@ -89,7 +96,8 @@ protected:
         //Unit Conversion: 6ft = 1828mm//
 
 
-        if (ptr->point.x != -9999 || ptr->point.y != -9999 || ptr->point.z != -9999)
+        //if (ptr->point.x != -9999 || ptr->point.y != -9999 || ptr->point.z != -9999)
+        if (ptr->point.x > -610 && ptr->point.x < 610)
         {
            person_point = true; 
            ROS_INFO("Got Person Point");
@@ -101,6 +109,8 @@ protected:
            else
            {
             person_distance_threshold_high = false;
+            // ac.cancelGoal();
+            // ROS_INFO("Canceling All Goals");
            }
 
         }
@@ -111,6 +121,29 @@ protected:
         }
     }
 
+
+    //Subscribing to the Person Location in MAP 
+    void locationTagMAPCallBack(const rehab_person_loc::location_info::ConstPtr &ptr)
+    {
+        if(ptr->person_location != "Away")
+        {
+            std::cout<<"person detected!!!!!!!!!"<<std::endl;
+            person_inside_map = true;
+        }
+
+    }
+
+    void personinMAPCallBack(const geometry_msgs::PointStamped::ConstPtr &ptr)
+    {
+        if(person_inside_map == true)
+        //if (ptr->point.x != -9999 || ptr->point.y != -9999 || ptr->point.z != -9999)
+        {
+            person_loc_in_MAP.point.x = ptr->point.x;
+            person_loc_in_MAP.point.y = ptr->point.y;
+            person_loc_in_MAP.point.z = ptr->point.z;
+        }
+
+    }
 
     void execute_callback(const behavior_tree_core::BTGoalConstPtr &goal)
     {
@@ -152,28 +185,62 @@ protected:
             while(person_point == false)
             {
                 msg.linear.x = 0.0;
-                msg.angular.z = 0.1;   //0.3 for simulations if the robot moves too slow
+                msg.angular.z = 0.2;   //0.3 for simulations if the robot moves too slow
                 pub_vel.publish(msg);
+
+                ros::Duration(1.5).sleep();
+
+                msg.linear.x = 0.0;
+                msg.angular.z = 0.0;   //0.3 for simulations if the robot moves too slow
+                pub_vel.publish(msg);  
+
+                ros::Duration(1.5).sleep();
+
             }
             
             feedback_msg.data = "Person Found";
             pub_feedback.publish(feedback_msg);
 
+
+            // // Give goal of the Person to the robot
+            // move_base_goal.target_pose.header.frame_id = "map";
+            // move_base_goal.target_pose.header.stamp = ros::Time::now();
+
+            // move_base_goal.target_pose.pose.position.x = person_loc_in_MAP.point.x;
+            // move_base_goal.target_pose.pose.position.y = person_loc_in_MAP.point.y;
+            // move_base_goal.target_pose.pose.orientation.w = 1.0;
+
+
+            // ROS_INFO("**Sending Person Location goal");
+            // ac.sendGoal(move_base_goal);
+
+            // feedback_msg.data = "Goal Sent";
+            // pub_feedback.publish(feedback_msg);
+            // ac.waitForResult();
+
+
             while (person_distance_threshold_high == true)
             {
+                feedback_msg.data = "Moving towards person";
+                pub_feedback.publish(feedback_msg);
                 msg.angular.z = 0;
                 msg.linear.x = 0.1;
                 pub_vel.publish(msg);
+
+                // //Give goal of the Person to the robot
+                // move_base_goal.target_pose.header.frame_id = "map";
+                // move_base_goal.target_pose.header.stamp = ros::Time::now();
+
+                // move_base_goal.target_pose.pose.position.x = person_loc_in_MAP.point.x;
+                // move_base_goal.target_pose.pose.position.y = person_loc_in_MAP.point.y;
+
+                // ROS_INFO("**Sending Person Location goal");
+                // ac.sendGoal(move_base_goal);
+                // ac.waitForResult();
             }
             msg.linear.x = 0.0;
             msg.angular.z = 0.0;
-            pub_vel.publish(msg);
-            // msg.angular.z = 0.0;
-            // pub_vel.publish(msg);
-            // msg.angular.z = 0.0;
-            // pub_vel.publish(msg);
-            // msg.angular.z = 0.0;
-            // pub_vel.publish(msg);
+            pub_vel.publish(msg); //uncomment for direct velocity publishing
 
             set_status(SUCCESS);
             feedback_msg.data = "";
